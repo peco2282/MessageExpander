8a42e89d00eabcc04076a59969f473ead0b96e28
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-06-12 19:44:29 +0900

    edit package name

--- expand/__init__.py
+++ expand/__init__.py
@@ -3,6 +3,6 @@ This is an extention for discord message for python.
 look at README file if you need more description.
 """
 
-from expander.expander import *
+from expand.expand import *
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
